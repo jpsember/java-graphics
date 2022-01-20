d47c5457a1a33cefb0bc8fd80aeb3fb8cfb48eb0
Jeff Sember <jpsember@gmail.com>
2022-01-20 14:31:46 -0800

    Issue #5: added support for centripetal variant of catmull rom splines

--- src/main/java/js/graphics/PolygonSmoother.java
+++ src/main/java/js/graphics/PolygonSmoother.java
@@ -11,7 +11,18 @@ import static js.base.Tools.*;
 import java.util.List;
 
 /**
+ * <pre>
+ * 
  * Smooths a polygon by generating Catmull-Rom splines for its edges
+ * 
+ * Notes
+ * -----------------------------------------------------------------------------------
+ * If the polygons are quite small, there can be unwanted visual effects (staircasing)
+ * due to quantization of the floating point intermediate results to (integer-valued) Polygons.
+ * 
+ * Does not yet support open polygons.
+ * 
+ * </pre>
  */
 public final class PolygonSmoother extends BaseObject {
 
@@ -22,18 +33,33 @@ public final class PolygonSmoother extends BaseObject {
     return this;
   }
 
+  /**
+   * (not applicable if doing centripetal version)
+   */
   public PolygonSmoother withStepSize(float stepSize) {
     mStepSize = stepSize;
     return this;
   }
 
+  /**
+   * (not applicable if doing centripetal version)
+   */
   public PolygonSmoother withInsetDistance(float distance) {
     mInsetDistance = distance;
     return this;
   }
 
+  /**
+   * (not applicable if doing centripetal version)
+   */
   public PolygonSmoother withTau(float tau) {
     mTau = tau;
+    return this;
+  }
+
+  public PolygonSmoother withCentripetalFactor(float factor, int stepCount) {
+    mCentripetalFactor = factor;
+    mCentripetalSteps = stepCount;
     return this;
   }
 
@@ -47,6 +73,17 @@ public final class PolygonSmoother extends BaseObject {
     Polygon sourcePolygon = checkNotNull(mSourcePolygon, "no polygon provided");
     checkArgument(sourcePolygon.isWellDefined(), "ill defined polygon");
     List<FPoint> src = preprocessVertices(sourcePolygon);
+    List<FPoint> target = arrayList();
+
+    if (mCentripetalFactor != 0)
+      centripetalCatmullRom(src, target);
+    else
+      catmullRom(src, target);
+
+    mResult = new Polygon(IPoint.convert(target));
+  }
+
+  private void catmullRom(List<FPoint> src, List<FPoint> target) {
 
     // See: https://www.cs.cmu.edu/~fp/courses/graphics/asst5/catmullRom.pdf
 
@@ -58,8 +95,6 @@ public final class PolygonSmoother extends BaseObject {
     final float c20 = 2 * t, c21 = t - 3, c22 = 3 - 2 * t, c23 = -t;
     final float c30 = -t, c31 = 2 - t, c32 = t - 2, c33 = t;
 
-    List<IPoint> ivertices = arrayList();
-
     for (int segIndex = 0; segIndex < src.size(); segIndex++) {
       FPoint pa = getMod(src, segIndex - 2);
       FPoint pb = getMod(src, segIndex - 1);
@@ -69,10 +104,14 @@ public final class PolygonSmoother extends BaseObject {
       // make the step size approximately constant by looking at the length of the edge.
       // BUT: is the length of the single segment a good estimate of the length of the curve generated?
 
+      float stepSize = mStepSize;
       float estimatedSegmentLength = MyMath.distanceBetween(pb, pc);
-      int stepCount = Math.max(1, Math.round(estimatedSegmentLength / mStepSize));
+      int stepCount = Math.max(1, Math.round(estimatedSegmentLength / stepSize));
       float stepIncrement = 1f / stepCount;
-      float stepAccum = 0;
+
+      // Not sure this has much effect - space steps so we're centered around t= 0.5
+      //
+      float stepAccum = (1 - stepCount * stepIncrement) / 2;
 
       for (int stepIndex = 0; stepIndex < stepCount; stepIndex++, stepAccum += stepIncrement) {
 
@@ -89,17 +128,79 @@ public final class PolygonSmoother extends BaseObject {
         float ix = f0 * pa.x + f1 * pb.x + f2 * pc.x + f3 * pd.x;
         float iy = f0 * pa.y + f1 * pb.y + f2 * pc.y + f3 * pd.y;
 
-        ivertices.add(new IPoint(ix, iy));
+        target.add(new FPoint(ix, iy));
       }
     }
-    mResult = new Polygon(ivertices);
+  }
+
+  private void centripetalCatmullRom(List<FPoint> src, List<FPoint> target2) {
+    // See: https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
+
+    for (int segIndex = 0; segIndex < src.size(); segIndex++) {
+      FPoint p0 = getMod(src, segIndex - 2);
+      FPoint p1 = getMod(src, segIndex - 1);
+      FPoint p2 = getMod(src, segIndex);
+      FPoint p3 = getMod(src, segIndex + 1);
+
+      float t0 = 0;
+      float t1 = tj(t0, p0, p1);
+      float t2 = tj(t1, p1, p2);
+      float t3 = tj(t2, p2, p3);
+
+      int stepTotal = mCentripetalSteps;
+      float stepSize = (t2 - t1) / stepTotal;
+      float t = t1 + stepSize / 2;
+
+      for (int stepIndex = 0; stepIndex < stepTotal; stepIndex++, t += stepSize) {
+
+        float a1p0 = (t1 - t) / (t1 - t0);
+        float a1p1 = (t - t0) / (t1 - t0);
+
+        float a2p1 = (t2 - t) / (t2 - t1);
+        float a2p2 = (t - t1) / (t2 - t1);
+
+        float a3p2 = (t3 - t) / (t3 - t2);
+        float a3p3 = (t - t2) / (t3 - t2);
+
+        float b1a1 = (t2 - t) / (t2 - t0);
+        float b1a2 = (t - t0) / (t2 - t0);
+
+        float b2a2 = (t3 - t) / (t3 - t1);
+        float b2a3 = (t - t1) / (t3 - t1);
+
+        float cb1 = (t2 - t) / (t2 - t1);
+        float cb2 = (t - t1) / (t2 - t1);
+
+        float a1x = a1p0 * p0.x + a1p1 * p1.x;
+        float a1y = a1p0 * p0.y + a1p1 * p1.y;
+
+        float a2x = a2p1 * p1.x + a2p2 * p2.x;
+        float a2y = a2p1 * p1.y + a2p2 * p2.y;
+
+        float a3x = a3p2 * p2.x + a3p3 * p3.x;
+        float a3y = a3p2 * p2.y + a3p3 * p3.y;
+
+        float b1x = b1a1 * a1x + b1a2 * a2x;
+        float b1y = b1a1 * a1y + b1a2 * a2y;
+
+        float b2x = b2a2 * a2x + b2a3 * a3x;
+        float b2y = b2a2 * a2y + b2a3 * a3y;
+
+        float cx = cb1 * b1x + cb2 * b2x;
+        float cy = cb1 * b1y + cb2 * b2y;
+
+        target2.add(new FPoint(cx, cy));
+      }
+    }
+  }
+
+  private float tj(float t, FPoint a, FPoint b) {
+    float sqdist = MyMath.squaredDistanceBetween(a, b);
+    float alpha = mCentripetalFactor;
+    return t + (float) Math.pow(sqdist, alpha);
   }
 
   private List<FPoint> preprocessVertices(Polygon p) {
-    todo(
-        "Have option to perform simplification (e.g. to eliminate collinear vertices)?  Or can we just pass in a simplified polygon?");
-    todo("Add support for open polygons");
-
     List<FPoint> result = arrayList();
 
     if (mInsetDistance == 0) {
@@ -134,5 +235,7 @@ public final class PolygonSmoother extends BaseObject {
   private float mTau = 0.5f;
   private float mStepSize = 1f;
   private float mInsetDistance;
+  private float mCentripetalFactor;
+  private int mCentripetalSteps;
   private Polygon mResult;
 }
